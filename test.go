30afbab2283bc053e7e2a515d4309af2090a8594
durianno1 <durianno1@aliyun.com>
2020-07-02 21:39:54 +0800

    internet

--- test.go
+++ test.go
@@ -1,14 +1,22 @@
-package main
-
-import (
-	"fmt"
-	"reflect"
-)
-
 func main() {
-	type a int
-	var s a = 5
-	v := reflect.ValueOf(&s)
-	v = v.Elem()
-	fmt.Println(v.Interface())
+    go spinner(100 * time.Millisecond)
+    const n = 45
+    fibN := fib(n) // slow
+    fmt.Printf("\rFibonacci(%d) = %d\n", n, fibN)
+}
+
+func spinner(delay time.Duration) {
+    for {
+        for _, r := range `-\|/` {
+            fmt.Printf("\r%c", r)
+            time.Sleep(delay)
+        }
+    }
+}
+
+func fib(x int) int {
+    if x < 2 {
+        return x
+    }
+    return fib(x-1) + fib(x-2)
 }
