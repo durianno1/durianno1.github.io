8fc043440155fb91f053a2a313bc48fd5e054f8a
durianno1 <durianno1@aliyun.com>
2020-07-27 16:42:57 +0800

    redis

--- test.go
+++ test.go
@@ -1,22 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
 func main() {
-    go spinner(100 * time.Millisecond)
-    const n = 45
-    fibN := fib(n) // slow
-    fmt.Printf("\rFibonacci(%d) = %d\n", n, fibN)
-}
-
-func spinner(delay time.Duration) {
-    for {
-        for _, r := range `-\|/` {
-            fmt.Printf("\r%c", r)
-            time.Sleep(delay)
-        }
-    }
-}
-
-func fib(x int) int {
-    if x < 2 {
-        return x
-    }
-    return fib(x-1) + fib(x-2)
+	fmt.Println(math.Pow(2, 31))
 }
